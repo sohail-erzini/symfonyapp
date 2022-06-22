c278b61c1556e1d1fbef59d3fb65c5041cd739d5
Sohail Erzini <sohail.erzini@gmail.com>
2022-06-23 00:53:20 +0100

    darklyandhomesetup

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ClientController extends AbstractController
+class HomeController extends AbstractController
 {
     /**
-     * @Route("/client", name="app_client")
+     * @Route("/", name="app_home")
      */
     public function index(): Response
     {
-        return $this->render('client/index.html.twig');
+        return $this->render('home/index.html.twig');
     }
 }
