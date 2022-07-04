8d03107fe85844b661589ebdf04d7ec443f5edd2
Sohail Erzini <sohail.erzini@gmail.com>
2022-07-04 20:10:08 +0100

    tache

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -345,5 +345,9 @@ class User implements UserInterface
         return $this;
     }
 
+    public function __toString() {
+        return $this->username;
+    }
+
 
 }
---
+++ src/Form/TacheType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tache;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TacheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('status')
+            ->add('priorite')
+            ->add('dateDebut')
+            ->add('dateFin')
+            ->add('dateAffectation')
+            ->add('dateModif')
+            ->add('phase')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tache::class,
+        ]);
+    }
+}
